85dada1251334ffc125ae579a26f1f46a8e14b1c
Harry <HarryMcLean11@gmail.com>
2025-04-27 16:51:52 +0100

    Loads data

--- train.py
+++ train.py
@@ -5,13 +5,23 @@ import torch.nn as nn
 
 
 class IMLONetwork(nn.Module):
-
+    # Set up Network
     def __init__(self):
         super().__init__()
 
+    def forward(self, x):
+        return x
+
 
 if __name__ == '__main__':
+    # Initialises Model
     model = IMLONetwork()
+
+    # sets transforms for images
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    # Downloads dataset
     trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+
+    # Loads dataset
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=True)
